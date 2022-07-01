80068c09afbb27df5c8552f9609d6d6f8d06c1ac
igor.righi <igor.righi@OTM-00194>
2022-07-01 11:10:58 -0300

    Adicionar arquivos de projeto.

---
+++ App02_Tarefa/Telas/Cadastro.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using App02_Tarefa.Modelos;
+
+namespace App02_Tarefa.Telas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Cadastro : ContentPage
+    {
+        private byte Prioridade { get; set; }
+
+        public Cadastro()
+        {
+            InitializeComponent();
+        }
+        public void PrioridadeSelectAction(object sender, EventArgs args)
+        {
+            var Stacks = SlPrioridades.Children;
+
+            foreach (var Linha in Stacks)
+            {
+                Label LblPrioridade = ((StackLayout)Linha).Children[1] as Label;
+                LblPrioridade.TextColor = Color.Gray;
+                LblPrioridade.BackgroundColor= Color.Transparent;
+
+
+            }
+
+           
+            FileImageSource Source = ((Image)((StackLayout)sender).Children[0]).Source as FileImageSource;
+            string Prioridade = Source.File.ToString().Replace("p", "").Replace(".ng","");
+           
+            this.Prioridade = byte.Parse(Prioridade);
+            if (this.Prioridade == 1)
+            {
+                ((Label)((StackLayout)sender).Children[1]).TextColor = Color.White;
+                ((Label)((StackLayout)sender).Children[1]).BackgroundColor = Color.Green;
+            }
+            if (this.Prioridade == 2)
+            {
+                ((Label)((StackLayout)sender).Children[1]).TextColor = Color.Black;
+                ((Label)((StackLayout)sender).Children[1]).BackgroundColor = Color.Yellow;
+            }
+            if (this.Prioridade == 3)
+            {
+                ((Label)((StackLayout)sender).Children[1]).TextColor = Color.White;
+                ((Label)((StackLayout)sender).Children[1]).BackgroundColor = Color.Orange;
+            }
+            if (this.Prioridade == 4)
+            {
+                ((Label)((StackLayout)sender).Children[1]).TextColor = Color.White;
+                ((Label)((StackLayout)sender).Children[1]).BackgroundColor = Color.Red;
+                
+            }
+        }
+        public void SalvarAction(object sender, EventArgs args)
+        {
+            bool ErroExiste = false;
+            if (TxtNome.Text == null || TxtNome.Text.Trim().Length <= 0)
+            {
+                DisplayAlert("ERRO", "Nome não preenchido", "OK");
+                ErroExiste = true;
+            }
+            if (Prioridade <= 0)
+            {
+                DisplayAlert("ERRO", "Prioridade não foi informada", "OK");
+                ErroExiste = true;
+            }
+            if(ErroExiste == false)
+            {
+                Tarefa tarefa = new Tarefa();
+                tarefa.Nome = TxtNome.Text.Trim();
+                tarefa.Prioridade = this.Prioridade;
+
+                new GerenciadorTarefa().Salvar(tarefa);
+                App.Current.MainPage = new NavigationPage(new Inicio());
+                
+                
+            }
+        }
+    }
+}
---
+++ App02_Tarefa/Telas/Inicio.xaml.cs
@@ -0,0 +1,125 @@
+﻿using App02_Tarefa.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_Tarefa.Telas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inicio : ContentPage
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+            CultureInfo culture = new CultureInfo("pt-BR");
+            string data = DateTime.Now.ToString("dddd, dd {0} MMMM {0} yyyy", culture);
+            DataHoje.Text = string.Format(data, "de");
+            CarregarTarefas();
+        }
+        public void ActionGoCadastro(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Cadastro());
+        }
+        private void CarregarTarefas()
+        {
+            SLTarefas.Children.Clear();
+            List<Tarefa> lista = new GerenciadorTarefa().Listagem();
+
+            int i = 0;
+            foreach(Tarefa tarefa in lista)
+            {
+                LinhaStackLayout(tarefa, i);
+                i++;
+            }
+        }
+        public void LinhaStackLayout(Tarefa tarefa, int index)
+        {
+            Image Delete = new Image()
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Source = ImageSource.FromFile("Delete.png")
+            };
+           
+            TapGestureRecognizer DeleteTap = new TapGestureRecognizer();
+            DeleteTap.Tapped += delegate
+            {
+                new GerenciadorTarefa().Deletar(index);
+                CarregarTarefas();
+            };
+
+            Delete.GestureRecognizers.Add(DeleteTap);
+
+
+            Image Prioridade = new Image()
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Source = ImageSource.FromFile("p"+tarefa.Prioridade+".png") 
+            };
+            View StackCentral = null;
+            if(tarefa.DataFinalizacao == null)
+            {
+                StackCentral = new Label()
+            {
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                Text = tarefa.Nome
+            };
+
+            }
+            else
+            {
+                StackCentral = new StackLayout()
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Spacing = 0,
+                    HorizontalOptions = LayoutOptions.FillAndExpand,
+                    
+                };
+                ((StackLayout)StackCentral).Children.Add(new Label() { Text = tarefa.Nome, TextColor = Color.Gray });
+                ((StackLayout)StackCentral).Children.Add(new Label() { Text = "Finalizado em: "+tarefa.DataFinalizacao.Value.ToString("dd/MM/yyyy - hh:mm")+"h", TextColor = Color.Gray, FontSize = 10 });
+            }
+
+
+            
+            Image Check = new Image()
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Source = ImageSource.FromFile("CheckOff.png")
+            };
+
+            if(tarefa.DataFinalizacao != null)
+            {
+                Check.Source = ImageSource.FromFile("CheckOn.png");
+            }
+
+            TapGestureRecognizer CheckTap = new TapGestureRecognizer();
+            CheckTap.Tapped += delegate
+            {
+                new GerenciadorTarefa().Finalizar(index,tarefa);
+                CarregarTarefas();
+            };
+
+            Check.GestureRecognizers.Add(CheckTap);
+
+            StackLayout linha = new StackLayout()
+            {
+                Orientation = StackOrientation.Horizontal,
+                Spacing = 15
+            };
+            linha.Children.Add(Check);
+            linha.Children.Add(StackCentral);
+            linha.Children.Add(Prioridade);
+            linha.Children.Add(Delete);
+
+            SLTarefas.Children.Add(linha);
+
+
+        }
+    }   
+}
